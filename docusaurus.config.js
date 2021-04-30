0538731952b3c14c1c24b3a29a9ffdc6b7cc29ed
Eduardo Asenjo <eduardo.asenjo@pexlify.com>
2021-04-30 14:10:37 +0100

    refactor: apply prettier

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,79 +1,76 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'SF Docs generator',
-  tagline: 'A non-official sfdx plugin to generate documentation of your projects based in the source code of your repository.',
-  url: 'https://sfdocs-test.netlify.app',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  title: "SF Docs generator",
+  tagline:
+    "A non-official sfdx plugin to generate documentation of your projects based in the source code of your repository.",
+  url: "https://sfdocs.netlify.app",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'SFDocs',
+      title: "SFDocs",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: "My Site Logo",
+        src: "img/logo.svg",
       },
       items: [
         {
-          type: 'doc',
-          docId: 'intro',
-          position: 'left',
-          label: 'Documentation',
+          type: "doc",
+          docId: "intro",
+          position: "left",
+          label: "Documentation",
         },
         // {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/raspikabek/sfdocs-sfdx-plugin',
-          position: 'right',
-          className: 'header-github-link',
-          'aria-label': 'GitHub repository'
+          href: "https://github.com/raspikabek/sfdocs-sfdx-plugin",
+          position: "right",
+          className: "header-github-link",
+          "aria-label": "GitHub repository",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'How to install',
-              to: '/docs/intro',
+              label: "How to install",
+              to: "/docs/intro",
             },
             {
-              label: 'Commands',
-              to: '/docs/intro',
+              label: "Commands",
+              to: "/docs/intro",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
+              label: "Plugin's GitHub",
+              href: "https://github.com/raspikabek/sfdocs-sfdx-plugin",
             },
             {
-              label: 'Plugin\'s GitHub',
-              href: 'https://github.com/raspikabek/sfdocs-sfdx-plugin',
-            },
-            {
-              label: 'Site\'s GitHub',
-              href: 'https://github.com/raspikabek/sfdocs-site',
+              label: "Site's GitHub",
+              href: "https://github.com/raspikabek/sfdocs-site",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
               html: `
               <a href="https://www.buymeacoffee.com/raspikabek" target="_blank">
                 <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="width: 11em !important;" >
               </a>
-              `
+              `,
             },
             {
               html: `
@@ -81,7 +78,7 @@ module.exports = {
                   <img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg" alt="Deploys by Netlify" />
                 </a>
               `,
-            }
+            },
           ],
         },
       ],
@@ -90,22 +87,22 @@ module.exports = {
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/raspikabek/sfdocs-site/edit/master/website/',
+            "https://github.com/raspikabek/sfdocs-site/edit/master/website/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/raspikabek/sfdocs-site/edit/master/website/blog/',
+            "https://github.com/raspikabek/sfdocs-site/edit/master/website/blog/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -1,40 +1,43 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './HomepageFeatures.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./HomepageFeatures.module.css";
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Easy to Use",
+    Svg: require("../../static/img/undraw_docusaurus_mountain.svg").default,
     description: (
       <>
-        Build it anywhere, anytime in seconds. Execute it from your CI/CD and integrate it with your documentation page
+        Build it anywhere, anytime in seconds. Execute it from your CI/CD and
+        integrate it with your documentation page.
       </>
     ),
   },
   {
-    title: 'Open Source',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
+    title: "Open Source",
+    Svg: require("../../static/img/undraw_docusaurus_tree.svg").default,
     description: (
       <>
-        Free and open source forever! You only need sfdx-cli to install this plugin.
+        Free and open source forever! You only need sfdx-cli to install this
+        plugin.
       </>
     ),
   },
   {
-    title: 'Never stop developing',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
+    title: "Never stop developing",
+    Svg: require("../../static/img/undraw_docusaurus_react.svg").default,
     description: (
       <>
-        Focus in your development and new features. Stop dealing with incomplete or outdated documentation.
+        Focus in your development and new features. Stop dealing with incomplete
+        or outdated documentation.
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} alt={title} />
       </div>
